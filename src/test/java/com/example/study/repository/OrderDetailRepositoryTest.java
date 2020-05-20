906542082772a2fe1edec3fba47022aab4843857
chimchak <sksguswoek1@gmail.com>
2020-05-21 00:19:08 +0900

    set Jpa Relationship - #1 create Item table and order_detail table

---
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.OrderDetail;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class OrderDetailRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void create() {
+        OrderDetail orderDetail = new OrderDetail();
+
+        orderDetail.setOrderAt(LocalDateTime.now());
+        orderDetail.setUserId(1L);
+        orderDetail.setItemId(1L);
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+        assertNotNull(newOrderDetail);
+    }
+
+}
