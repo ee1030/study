cb389eb55b007f3e2f0ba051054f8b1cba962e36
chimchak <sksguswoek1@gmail.com>
2020-05-21 19:27:56 +0900

    added QueryMethod

--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -23,6 +23,13 @@ public class Item {
 
     private String content;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
+    // LAZY = 지연로딩, EAGER = 즉시로딩
+    // LAZY = SELECT * FROM USER WHERE id = ?
+    // EAGER = -> 데이터가 많을 때 모든 데이터를 조인하기 때문에 성능 저하를 가져올 수 있음
+    // EAGER 는 1:1 연관관계에 있어 한건만 존재할때 사용 추천
+    // item_id = order_detail.item_id
+    // user_id = order_detail.user_id
+    // where item.id = ?
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "item")
     private List<OrderDetail> orderDetailList;
 }
