026c82afd90a130f0eb7f384764389baa67fb531
chimchak <sksguswoek1@gmail.com>
2020-06-03 21:07:01 +0900

    added 사용자 API update, delete

--- src/main/java/com/example/study/service/UserApiLogicService.java
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -55,13 +55,44 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
 
     @Override
     public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+        // 1. data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. id -> user 데이터 찾기
+        Optional<User> optional = userRepository.findById(userApiRequest.getId());
+
+        optional.map(user -> {
+            // 3. update
+            user.setAccount(userApiRequest.getAccount())
+                    .setPassword(userApiRequest.getPassword())
+                    .setStatus(userApiRequest.getStatus())
+                    .setPhoneNumber(userApiRequest.getPhoneNumber())
+                    .setEmail(userApiRequest.getEmail())
+                    .setRegisteredAt(userApiRequest.getRegisteredAt())
+                    .setUnregisteredAt(userApiRequest.getUnregisteredAt());
+            return user;
+        })
+        .map(user -> userRepository.save(user))
+        .map(updateUser -> response(updateUser))           // 4. userApiResponse
+        .orElseGet(()->Header.ERROR("데이터 없음"));
+
         return null;
     }
 
     @Override
     public Header delete(Long id) {
-        return null;
-    }
+        // 1. id -> repository -> user
+        Optional<User> optional = userRepository.findById(id);
+
+        // 2. repository delete
+        // 3. response return
+        return optional.map(user -> {
+            userRepository.delete(user);
+            return Header.OK();
+        })
+        .orElseGet(() -> Header.ERROR("데이터 없음"));
+
+}
 
     private Header<UserApiResponse> response(User user) {
         // user -> userApiResponse
